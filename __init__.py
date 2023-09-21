47891e278baf285f60f8092d4b7c947adafcec8f
suryasahiba <sahibas@iitbhilai.ac.in>
2023-09-21 22:42:54 +0530

    first commit

---
+++ __init__.py
@@ -0,0 +1,36 @@
+import json
+from flask import Flask, render_template, request, jsonify
+import math
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("InputOutput.html")        
+
+@app.route("/add", methods=["POST"])
+def ADD(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    sum=a+b
+    response = "sum = " + str(sum)
+    return response
+#add your functions below
+
+@app.route("/your_function", methods=["POST"])
+def your_function(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    
+    a=int(jsonObj['N1'])
+    b=int(jsonObj['N2'])
+    # write code for your_function
+    response = "your_function"
+    return response
+
+
+if __name__== "__main__":
+    app.run()
